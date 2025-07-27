3cfabeeb5edc8fcf4105755235efec2f76fbf7d7
Alejandro R <arc19881@gmail.com>
2025-07-28 00:58:13 +0200

    loading fix

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const API = axios.create({
-  baseURL: 'https://mv-facturacion-back.onrender.com/api/',
+  baseURL: import.meta.env.VITE_API_BASE_URL,
   withCredentials: true, // Puedes dejar esto si usas cookies, pero no es necesario para Token Auth
 });
 
